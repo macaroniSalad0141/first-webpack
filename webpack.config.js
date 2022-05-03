58008e1eed301511d4dbb56990b553eb1f5ef765
macaloniSalad0141 <azure01wataru@gmail.com>
2022-05-03 16:59:25 +0900

    ファイル構成の整理

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { Template } = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/javascripts/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'main.js'
+        filename: 'javascripts/main.js'
     },
     module: {
         rules: [
@@ -25,9 +26,12 @@ module.exports = {
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: './stylesheets/my.css',
+        }),
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
+        new CleanWebpackPlugin(),
     ],
 }
